e822fd074cb3ef0e68ac641bc4814f94ab8e124b
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2023-01-03 09:35:03 +0700

    Done seeding

--- NSC_Project/NSC_Project/SeedData/SeedData.cs
+++ NSC_Project/NSC_Project/SeedData/SeedData.cs
@@ -59,60 +59,71 @@ namespace NSC_Project.SeedData
                     new AirportFrom
                     {
                         Name = "VNA-009",
-                        Address = "Vinh(VII)-Sân Bay Vinh", 
+                        Address = "Vinh(VII)-Sân Bay Vinh",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-010",
                         Address = "Tuy Hoà(TBB)-Sân Bay Tuy Hoà",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-011",
                         Address = "Chiang Mai(CNX)-Sân Bay Quốc Tế Chiang Mai",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-012",
                         Address = "Ko Samui(USM)-Sân Bay Samui",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-013",
                         Address = "Udon Thani(UTH)-Sân Bay Udon Thani",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-014",
                         Address = "Krabi(KBV)-Sân Bay Krabi",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-015",
                         Address = "Udon Ratchathani(UBP)-Sân Bay Udon Ratchathani",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-016",
                         Address = "Buri Ram(BFV)-Sân Bay Buri Ram",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-017",
                         Address = "Bangkok(BKK)-Sân Bay Suvarnabhumi",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-018",
                         Address = "Singapore(SIN)-Sân Bay Singapore Changi",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-019",
                         Address = "Pattaya(UTP)-Sân Bay Quốc tế U-Tapao",
                     },
                     new AirportFrom
                     {
+
                         Name = "VNA-020",
                         Address = "Phuket(HKT)-Sân Bay Quốc Tế Phuket",
                     }
@@ -171,56 +182,67 @@ namespace NSC_Project.SeedData
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-010",
                         Address = "Phuket(HKT)-Sân Bay Quốc Tế Phuket",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-011",
                         Address = "Hồ Chí Minh (SGN)-Sân Bay Tân Sơn Nhất",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-012",
                         Address = "Hà Nội(HAN)-Sân Bay Nội Bài",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-013",
                         Address = "UNha Trang(CXR)-Sân Bay Cam Ranh",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-014",
                         Address = "Phú Quốc(PQC)-Sân Bay Phú Quốc",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-015",
                         Address = "Quảng Nam(VCL)-Sân Bay Chu Lai",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-016",
                         Address = "Cần Thơ(VCA)-Sân Bay Cần Thơ",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-017",
                         Address = "Hải Phòng(HPH)-Sân Bay Cát Bi",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-018",
                         Address = "QUảng Ninh(VDO)-Sân Bay Vân Đồn",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-019",
                         Address = "Vinh(VII)-Sân Bay Vinh",
                     },
                     new AirportTo
                     {
+
                         Name = "VNA-020",
                         Address = "Tuy Hoà(TBB)-Sân Bay Tuy Hoà",
                     }
@@ -244,6 +266,136 @@ namespace NSC_Project.SeedData
                         Logo = ""
                     }
                 );
+                context.SaveChanges();
+
+                context.Trip.AddRange(
+                        new Trip
+                        {
+                            StartDate = DateTime.Now,
+                            FinishDate = DateTime.Now,
+                            FlightTime = 2,
+                            Status = 0,
+                            PlaneId = 1,
+                            FlightRouteId = 1,
+                            AirlineCompanyId = 1
+                        }
+                    );
+                context.SaveChanges();
+
+                context.Plane.AddRange(
+                        new Plane
+                        {
+                            Name = "Boeing 787"
+                        },
+                        new Plane
+                        {
+                            Name = "Airbus A350"
+                        },
+                        new Plane
+                        {
+                            Name = "Airbus A330"
+                        },
+                        new Plane
+                        {
+                            Name = "Airbus A321"
+                        },
+                        new Plane
+                        {
+                            Name = "Boeing 737 MAX 200"
+                        },
+                        new Plane
+                        {
+                            Name = "Airbus A321"
+                        },
+                        new Plane
+                        {
+                            Name = "Airbus A321 neo"
+                        }
+                    );
+                context.SaveChanges();
+                context.FlightRoute.AddRange(
+                        new FlightRoute
+                        {
+                            AirportFromId = 1,
+                            AirportToId = 2,
+                        }
+                    );
+                context.SaveChanges();
+                context.TicketClass.AddRange(
+                        new TicketClass
+                        {
+                            Name = "Eco"
+                        },
+                        new TicketClass
+                        {
+                            Name = "Economy Saver"
+                        },
+                        new TicketClass
+                        {
+                            Name = "Deluxe"
+                        },
+                        new TicketClass
+                        {
+                            Name = "SkyBoss"
+                        }
+                    );
+                context.SaveChanges();
+
+                context.Fare.AddRange(
+                        new Fare
+                        {
+                            Qty = 50,
+                            Price = 1270120,
+                            TripId= 1,
+                            TicketClassId= 1
+                        },
+                         new Fare
+                         {
+                             Qty = 50,
+                             Price = 1670120,
+                             TripId = 1,
+                             TicketClassId = 1
+                         },
+                          new Fare
+                          {
+                              Qty = 50,
+                              Price = 3270120,
+                              TripId = 1,
+                              TicketClassId = 1
+                          },
+                           new Fare
+                           {
+                               Qty = 50,
+                               Price = 5270120,
+                               TripId = 1,
+                               TicketClassId = 1
+                           }
+                    );
+                context.SaveChanges();
+
+                context.Ticket.AddRange(
+                        new Ticket
+                        {
+                            FareId= 1,
+                            TripId= 1
+                        },
+                        new Ticket
+                        {
+                            FareId = 2,
+                            TripId = 1
+                        },
+                        new Ticket
+                        {
+                            FareId = 3,
+                            TripId = 1
+                        },
+                        new Ticket
+                        {
+                            FareId = 4,
+                            TripId = 1
+                        }
+                    );
+                context.SaveChanges();
             }
         }
     }
